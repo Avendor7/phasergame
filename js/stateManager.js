f2d5254c9b756026ba788efaf5ea36c94fcc268d
stephen <steve.wiggins7@gmail.com>
2018-01-05 22:52:02 -0500

    full screen, add npm packages

--- js/stateManager.js
+++ js/stateManager.js
@@ -1,12 +1,10 @@
-var w = 640,
-  h = 480;
+//var w = 640, h = 480;
 
-/*
-For Fullscreen put this code:
+
+//For Fullscreen put this code:
 
 var w = window.innerWidth * window.devicePixelRatio,
     h = window.innerHeight * window.devicePixelRatio;
-*/
 
 var game = new Phaser.Game(w, h, Phaser.AUTO, 'gameContainer');
 
