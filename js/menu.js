b841c5b8aa062f0e15297b36aa5a776d9edf2aa2
stephen <steve.wiggins7@gmail.com>
2018-01-05 23:49:45 -0500

    comment out input for now

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ var menuState = {
         //game.add.plugin(Phaser.Plugin.Debug);
         //game.add.plugin(Phaser.Plugin.Inspector);
         //game.add.plugin(PhaserSuperStorage.StoragePlugin);
-        game.add.plugin(PhaserInput.Plugin);
+        //game.add.plugin(PhaserInput.Plugin);
         
         game.add.image(95, 150, 'boiler-logo');
 
